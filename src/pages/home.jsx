35f43dabf1d13bbda36ab072190e557795dd6ca5
vaaghu <vignesh021102@gmail.com>
2023-10-11 22:55:22 +0530

    event info

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -21,7 +21,32 @@ export default function Home({ navigate }) {
           Nov 22, 2023 • Chennai, Tamil Nadu, India
         </p>
       </div>
-      <div>some info</div>
+      <div className={homeStyle.eventCardContainer}>
+        <div className={homeStyle.eventCard}>
+          <p>ENGAGEMENT (NICHIATHARTHAM) CEREMONY</p>
+          <p>NOVEMBER 22, 2023 11:00–11:45</p>
+          <p>
+            Sri Krishnaswamy Kalyana Mandapam 1B Hindi Prachar Saba Road,
+            Chennai, TN, 600017, India
+          </p>
+        </div>
+        <div className={homeStyle.eventCard}>
+          <p>RECEPTION</p>
+          <p>NOVEMBER 22, 2023 11:00–11:45</p>
+          <p>
+            Sri Krishnaswamy Kalyana Mandapam 1B Hindi Prachar Saba Road,
+            Chennai, TN, 600017, India
+          </p>
+        </div>
+        <div className={homeStyle.eventCard}>
+          <p>WEDDING (MUHURTHAM)</p>
+          <p>NOVEMBER 22, 2023 11:00–11:45</p>
+          <p>
+            Sri Krishnaswamy Kalyana Mandapam 1B Hindi Prachar Saba Road,
+            Chennai, TN, 600017, India
+          </p>
+        </div>
+      </div>
       <div className={homeStyle.cardContainer}>
         {cardsInfo.map((card) => {
           return <CardComp cardInfo={card} key={card.id} navigate={navigate} />;
